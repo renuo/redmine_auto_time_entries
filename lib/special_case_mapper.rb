45e423e5a565f4ed8985f4efdd51222a0035b8b3
Lukas Elmer <lukas.elmer@gmail.com>
2013-08-15 14:19:12 +0200

    Update special_case_mapper.rb

--- lib/special_case_mapper.rb
+++ lib/special_case_mapper.rb
@@ -1,6 +1,6 @@
 class SpecialCaseMapper
   def map!(assigner)
-    if assigner.comment.start_with?('Weekly Renuo Meeting')
+    if assigner.comment.downcase.start_with?('internal Weekly Meeting')
       assigner.issue_id = 965
       assigner.activity = "Meetings"
     end
