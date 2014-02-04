03d072f97aca7fe7ca91c126a47a382b0f05cf25
Lukas Elmer <lukas.elmer@gmail.com>
2014-02-04 23:00:45 +0100

    Update special_case_mapper.rb

--- lib/special_case_mapper.rb
+++ lib/special_case_mapper.rb
@@ -1,8 +1,8 @@
 class SpecialCaseMapper
   def map!(assigner)
     s = assigner.comment.downcase
-    if s.start_with?('internal weekly meeting') || s.start_with?('weekly meeting')
-      assigner.issue_ids << 1117
+    if s.start_with?('internal weekly meeting') || s.start_with?('weekly meeting') || s.start_with?('Wöchentliches Meeting')
+      assigner.issue_ids << 1339
       assigner.activity = "Meetings"
     elsif s.start_with?('internal entwicklung bills')
       assigner.issue_ids << 831
